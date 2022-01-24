7a44bbb6e821cd0d90058b3e25db9b9131a2e609
Marcel Burda <marcelburda123@gmail.com>
2022-01-24 18:59:03 +0100

    first commit

---
+++ compile.bat
@@ -0,0 +1,16 @@
+pdflatex main.tex
+
+rem verzeichnis aufräumen
+del "%~dp0\main.aux"
+del "%~dp0\main.bbl"
+del "%~dp0\main.blg"
+del "%~dp0\main.idx"
+del "%~dp0\main.lof"
+del "%~dp0\main.log"
+del "%~dp0\main.lot"
+del "%~dp0\main.nlo"
+del "%~dp0\main.out"
+del "%~dp0\main.tdo"
+del "%~dp0\main.toc"
+del "%~dp0\Bericht.tps"
+del "%~dp0\main.synctex.gz"
